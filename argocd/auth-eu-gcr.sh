6d24003d9b7a6631662d140948fc8659a9b75c24
Andrei Chenchik <andrei@chenchik.me>
2022-01-22 22:46:29 +0100

    update auth script

--- argocd/auth-eu-gcr.sh
+++ argocd/auth-eu-gcr.sh
@@ -2,13 +2,18 @@
 
 set -e
 
-kubectl delete secret docker-registry eu-gcr-auth || true
+if [ "$GCRJSONAUTH" = "" ]; then
+	echo "Please provide GCR Auth JSON in GCRJSONAUTH env"
+	exit 1
+fi
 
-kubectl create secret docker-registry eu-gcr-auth \
+kubectl delete secret eu-gcr-io || true
+
+kubectl create secret docker-registry eu-gcr-io \
 	--docker-server=eu.gcr.io \
 	--docker-username=_json_key \
 	--docker-password="$GCRJSONAUTH" \
 	--docker-email=andrei@chenchik.me
 
 kubectl patch serviceaccount default \
-	-p '{"imagePullSecrets": [{"name": "eu-gcr-auth"}]}'
+	-p '{"imagePullSecrets": [{"name": "eu-gcr-io"}]}'
