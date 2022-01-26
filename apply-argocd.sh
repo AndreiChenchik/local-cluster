111ee9ec1e6c1794582247ca8cbaedbe04ee46c0
Andrei Chenchik <andrei@chenchik.me>
2022-01-26 10:53:30 +0100

    update apply script with emoji

--- apply-argocd.sh
+++ apply-argocd.sh
@@ -3,11 +3,11 @@
 set -e
 
 if [ "$ARGOPASS" = "" ]; then
-	echo "Please provide password for ArgoCD admin user in ARGOPASS env"
+	echo "👎 Please provide password for ArgoCD admin user in ARGOPASS env"
 	exit 1
 fi
 
-echo "\nApplying ArgoCD to argocd namespace"
+echo "\n🧬 Apply ArgoCD to argocd namespace"
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 helm upgrade --install \
@@ -15,7 +15,7 @@ helm upgrade --install \
   --set 'server.extraArgs={--insecure}' \
   --create-namespace --wait
 
-echo "\nUpdating the password"
+echo "\n🔓 Update the ArgoCD password"
 new_password=$(htpasswd -bnBC 10 "" $ARGOPASS | tr -d ':\n')
 secret_patch='{"stringData": {
     "admin.password": "'$new_password'",
@@ -23,15 +23,15 @@ secret_patch='{"stringData": {
   }}'
 kubectl -n argocd patch secret argocd-secret -p "$secret_patch"
 
-echo "\nCleaning argocd-initial-admin-secret"
-kubectl -n argocd delete secret argocd-initial-admin-secret
+echo "\n🗑  Clean default argocd-initial-admin-secret if exists"
+kubectl -n argocd delete secret argocd-initial-admin-secret || true
 
-echo "\nRestarting and wait for argocd-server"
+echo "\n🤖 Restart and wait for argocd-server"
 kubectl rollout restart deployment argocd-server -n argocd
 kubectl -n argocd wait deployment argocd-server --for=condition=available
 
-echo "\nApplying root application"
+echo "\n🚀 Apply root application"
 kubectl apply \
 	-f Application-Root.yaml
 
-echo "\nDone!"
+echo "\n🎖  Done!"
