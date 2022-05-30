54b00d27da6f0368f1dba8fe4ef61adedccac938
Andrei Chenchik <andrei@chenchik.me>
2022-05-30 22:16:19 +0200

    fmt

--- main.go
+++ main.go
@@ -7,19 +7,19 @@ import (
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-        _, err := yaml.NewConfigFile(ctx, "fluxComponents", &yaml.ConfigFileArgs{
-            File: "clusters/local/flux-system/gotk-components.yaml",
-        })
-        if err != nil {
-            return err
-        }
+		_, err := yaml.NewConfigFile(ctx, "fluxComponents", &yaml.ConfigFileArgs{
+			File: "clusters/local/flux-system/gotk-components.yaml",
+		})
+		if err != nil {
+			return err
+		}
 
 		_, err = yaml.NewConfigFile(ctx, "fluxSync", &yaml.ConfigFileArgs{
-            File: "clusters/local/flux-system/gotk-sync.yaml",
-        })
-        if err != nil {
-            return err
-        }
+			File: "clusters/local/flux-system/gotk-sync.yaml",
+		})
+		if err != nil {
+			return err
+		}
 
 		return nil
 	})
