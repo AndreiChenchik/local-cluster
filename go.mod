f5c88bbbffaa8fc13d12ca101853b9b1a6356ee2
Andrei Chenchik <andrei@chenchik.me>
2022-05-30 21:42:49 +0200

    Switch Pulumi from Python to Go
    Due to https://github.com/pulumi/pulumi-kubernetes/issues/1755

---
+++ go.mod
@@ -0,0 +1,8 @@
+module cluster
+
+go 1.16
+
+require (
+	github.com/pulumi/pulumi-kubernetes/sdk/v3 v3.19.2
+	github.com/pulumi/pulumi/sdk/v3 v3.33.2
+)
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/yaml"
+    "github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+        _, err := yaml.NewConfigFile(ctx, "fluxComponents", &yaml.ConfigFileArgs{
+            File: "clusters/local/flux-system/gotk-components.yaml",
+        })
+        if err != nil {
+            return err
+        }
+
+		_, err = yaml.NewConfigFile(ctx, "fluxSync", &yaml.ConfigFileArgs{
+            File: "clusters/local/flux-system/gotk-sync.yaml",
+        })
+        if err != nil {
+            return err
+        }
+
+		return nil
+	})
+}
