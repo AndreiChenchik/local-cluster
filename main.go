15501670949bcbc7a528c06382fe05dae977d862
Andrei Chenchik <andrei@chenchik.me>
2022-06-06 18:11:46 +0200

    fix path to init files

--- main.go
+++ main.go
@@ -8,14 +8,14 @@ import (
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := yaml.NewConfigFile(ctx, "fluxComponents", &yaml.ConfigFileArgs{
-			File: "cluster/local/flux-system/gotk-components.yaml",
+			File: "clusters/local/flux-system/gotk-components.yaml",
 		})
 		if err != nil {
 			return err
 		}
 
 		_, err = yaml.NewConfigFile(ctx, "fluxSync", &yaml.ConfigFileArgs{
-			File: "cluster/local/flux-system/gotk-sync.yaml",
+			File: "clusters/local/flux-system/gotk-sync.yaml",
 		})
 		if err != nil {
 			return err
